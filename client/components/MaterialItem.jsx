2f7d615dd0caacb7be1c120e1192416839c16645
Joel Reis <joelhreis@gmail.com>
2015-10-10 22:29:30 -0400

    material image refactor starting.

--- client/components/MaterialItem.jsx
+++ client/components/MaterialItem.jsx
@@ -15,37 +15,60 @@ MaterialItem = React.createClass({
     }
 
     return (
-      <div className="item">
-        <div className="ui tiny image">
-          <img src={this.props.item.image} />
-        </div>
-        <div className="content">
-          <div className="description">
-            <div className="ui relaxed divided list">
-              <div className="item">
-                <strong>{this.props.item.name}</strong>
-              </div>
-              <div className="item">
-                ${this.props.item.cost}
-              </div>
-              <div className="item">
-                <strong>Quantity</strong> {this.props.item.quantity} {this.props.item.unit}
-              </div>
+      <tr>
+        <td>
+          <h4 class="ui image header">
+            <div className="ui tiny image">
+              <img src={this.props.item.image} class="ui tiny rounded image" />
             </div>
-          </div>
-          <div className="extra">
-            <button className="ui right floated blue labeled icon button" onClick={this.deleteMaterial}>
-              <i className="icon edit"></i>
-              Update
-            </button>
-            <button className="ui right floated red labeled icon button">
-              <i className="icon trash"></i>
-              Delete
-            </button>
-            {tags}
-          </div>
-        </div>
-      </div>
+            <div class="content">
+              <strong>{this.props.item.name}</strong>
+              <div class="sub header">Human</div>
+            </div>
+          </h4>
+        </td>
+        <td>
+          ${this.props.item.cost}
+        </td>
+        <td>
+          {this.props.item.quantity} {this.props.item.unit}
+        </td>
+      </tr>
     )
+
+
+    // return (
+    //   <div className="item">
+    //     <div className="ui tiny image">
+    //       <img src={this.props.item.image} />
+    //     </div>
+    //     <div className="content">
+    //       <div className="description">
+    //         <div className="ui relaxed divided list">
+    //           <div className="item">
+    //             <strong>{this.props.item.name}</strong>
+    //           </div>
+    //           <div className="item">
+    //             ${this.props.item.cost}
+    //           </div>
+    //           <div className="item">
+    //             <strong>Quantity</strong> {this.props.item.quantity} {this.props.item.unit}
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <div className="extra">
+    //         <button className="ui right floated blue labeled icon button" onClick={this.deleteMaterial}>
+    //           <i className="icon edit"></i>
+    //           Update
+    //         </button>
+    //         <button className="ui right floated red labeled icon button">
+    //           <i className="icon trash"></i>
+    //           Delete
+    //         </button>
+    //         {tags}
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
   }
 })
--- client/components/MaterialList.jsx
+++ client/components/MaterialList.jsx
@@ -1,6 +1,25 @@
 MaterialList = React.createClass({
 
   renderMaterialItems() {
+    // return _.map(this.props.materials, (materialItem) => {
+    //   return (
+    //     <tr>
+    //       <td>
+    //         <h4 class="ui image header">
+    //           <img src="/images/avatar2/small/lena.png" class="ui mini rounded image">
+    //           <div class="content">
+    //             Lena
+    //             <div class="sub header">Human Resources
+    //           </div>
+    //         </div>
+    //       </h4></td>
+    //       <td>
+    //         22
+    //       </td>
+    //     </tr>
+    //   )
+    // })
+
     return _.map(this.props.materials, (materialItem) => {
       return (
         <MaterialItem key={materialItem._id} item={materialItem} />
@@ -10,9 +29,94 @@ MaterialList = React.createClass({
 
   render() {
     return (
-      <div className="ui divided items">
-        {this.renderMaterialItems()}
-      </div>
+      <table class="ui very basic collapsing celled table">
+        <thead>
+          <tr>
+            <th>Employee</th>
+            <th>Employee</th>
+          </tr>
+          <tbody>
+            {this.renderMaterialItems()}
+          </tbody>
+
+        </thead>
+      </table>
     )
+
+
+    // return (
+    //   <div className="ui divided items">
+    //     {this.renderMaterialItems()}
+    //   </div>
+    // )
   }
 })
+
+/*
+
+<table class="ui very basic collapsing celled table">
+  <thead>
+    <tr><th>Employee</th>
+    <th>Correct Guesses</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="/images/avatar2/small/lena.png" class="ui mini rounded image">
+          <div class="content">
+            Lena
+            <div class="sub header">Human Resources
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        22
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="/images/avatar2/small/matthew.png" class="ui mini rounded image">
+          <div class="content">
+            Matthew
+            <div class="sub header">Fabric Design
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        15
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="/images/avatar2/small/lindsay.png" class="ui mini rounded image">
+          <div class="content">
+            Lindsay
+            <div class="sub header">Entertainment
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        12
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="/images/avatar2/small/mark.png" class="ui mini rounded image">
+          <div class="content">
+            Mark
+            <div class="sub header">Executive
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        11
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+*/
