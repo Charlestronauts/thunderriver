1f75b58fed6990472d21b1d53bc6bcb117237918
Joel Reis <joelhreis@gmail.com>
2015-10-10 21:32:24 -0400

    Removed log.

--- client/components/MaterialList.jsx
+++ client/components/MaterialList.jsx
@@ -1,7 +1,6 @@
 MaterialList = React.createClass({
 
   renderMaterialItems() {
-    console.log(this.props.materials)
     return _.map(this.props.materials, (materialItem) => {
       return (
         <MaterialItem key={materialItem._id} item={materialItem} />
